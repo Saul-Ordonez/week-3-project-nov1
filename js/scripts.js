b801d88033c7e90a2630e9a13f8ecf953c402898
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 09:38:17 -0700

    add backend branch to test for NaN and negative numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 //back-end logic
-
+function inputCheck(userInput) {
+  if (isNaN(userInput)) {
+    alert('Please enter a real number');
+  }
+  else if (userInput < 0) {
+    alert('Please enter a positive number');
+  }
+}
 
 
 
@@ -7,8 +14,9 @@
 $(document).ready(function() {
   $('form#numbers').submit(function(event) {
     event.preventDefault();
+    var userInput = parseInt($('input#inputNumbers').val());
+    $('.output').append()
 
-  var numberInput = 
 
   });
 });
