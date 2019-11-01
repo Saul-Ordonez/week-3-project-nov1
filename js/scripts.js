1732a68f9125ea90bb0bbdc936738551a8f5d7ff
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 10:20:44 -0700

    add for loop branching for conditions needing to be met

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,30 @@ function inputCheck(userInput) {
     alert('Please enter a positive number');
   }
 }
+function beepboop(userInput) {
 
-var beep = 'Beep!';
-var boop = 'Boop!';
-var sorry = "I'm sorry Dave, I'm afraid I cannot do that.";
-var outputArray = [];
+  var beep = 'Beep!';
+  var boop = 'Boop!';
+  var sorry = "I'm sorry Dave, I'm afraid I cannot do that.";
+  var outputArray = [];
 
-
+  for (i = 0; i <= userInput; i++);
+    if ((i === 3) && (i !== 0)) {
+      outputArray.push(sorry);
+    }
+    else if (i.toString().includes('0')) {
+      outputArray.push(boop);
+    }
+    else if (i.toString().includes('1')) {
+      outputArray.push(beep);
+    }
+    else {
+      outputArray.push(i);
+    }
+  {
+    return outputArray;
+  }
+}
 
 
 //front-end logic
@@ -21,7 +38,7 @@ $(document).ready(function() {
   $('form#numbers').submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($('input#inputNumbers').val());
-
+    $('.output#result').text(beepboop(userInput));
 
 
   });
