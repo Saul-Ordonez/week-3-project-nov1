3903339b7b48aa4ead2cfce140de9bf9f5e6b407
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 08:50:42 -0700

    add basic html so webpage will receive user input

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//back-end logic
+
+
+
+
+//front-end logic
+$(document).ready(function() {
+  $('form#numbers').submit(function(event) {
+    event.preventDefault();
+
+  var numberInput = 
+
+  });
+});
