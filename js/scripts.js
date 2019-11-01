31e91abc21dd9efdb494dabeb55af5401de35951
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 14:24:17 -0700

    adjust syntax of output result

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 //Back-End logic//
-function checkInput(userInput) {
-  if (isNaN(userInput)) {
-    alert("Please enter a valid number.");
-  }
-  else if (userInput < 0) {
-    alert("Please enter a positive number.");
-  }
-}
+// function checkUserInput(userInput) {
+//   if (isNaN(userInput)) {
+//     alert('Please enter a valid number.');
+//     console.log(userInput);
+//   }
+//   else if (userInput < 0) {
+//     alert('Please enter a positive number.');
+//   }
+// }
 
 function beepBoop(userInput) {
   var outputArray = []
 
   for (i = 0; i <= userInput; i++) {
     if (i.toString().includes(3)) {
-      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that. ")
+      outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that. ")
     } else if (i.toString().includes(2)) {
-      outputArray.push("Boop! ");
+      outputArray.push(' Boop! ');
     } else if (i.toString().includes(1)) {
-      outputArray.push("Beep! ");
+      outputArray.push(' Beep! ');
     } else {
-      outputArray.push(i + ", ");
+      outputArray.push(' ' + i + ' ');
     }
   }
   return outputArray;
+
 }
 //Front-End logic//
 $(document).ready(function() {
