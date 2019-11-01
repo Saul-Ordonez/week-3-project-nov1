995b0c44cfae192eb25465e6d510b9161b991ed5
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 10:48:29 -0700

    troubleshoot javascript in the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,41 @@
 //back-end logic
-function inputCheck(userInput) {
-  if (isNaN(userInput)) {
-    alert('Please enter a real number');
-  }
-  else if (userInput < 0) {
-    alert('Please enter a positive number');
-  }
-}
-function beepboop(userInput) {
-
-  var beep = 'Beep!';
-  var boop = 'Boop!';
-  var sorry = "I'm sorry Dave, I'm afraid I cannot do that.";
+// function inputCheck(userInput) {
+//   if (isNaN(userInput)) {
+//     alert('Please enter a real number');
+//   }
+//   else if (userInput < 0) {
+//     alert('Please enter a positive number');
+//   }
+// }
+function beepBoop(userInput) {
+  // var beep = 'Beep!';
+  // var boop = 'Boop!';
+  // var sorry = "I'm sorry Dave, I'm afraid I cannot do that.";
   var outputArray = [];
 
-  for (i = 0; i <= userInput; i++);
-    if ((i === 3) && (i !== 0)) {
-      outputArray.push(sorry);
+  for (i = 0; i <= userInput; i++); {
+    if (i.toString().includes('3')) {
+      outputArray.push(" I'm sorry Dave, I'm afraid I cannot do that. ")
     }
     else if (i.toString().includes('0')) {
-      outputArray.push(boop);
+      outputArray.push(' boop! ');
     }
     else if (i.toString().includes('1')) {
-      outputArray.push(beep);
+      outputArray.push(' beep! ')
     }
     else {
       outputArray.push(i);
     }
-  {
-    return outputArray;
   }
+  return outputArray;
+console.log(outputArray);
 }
-
 
 //front-end logic
 $(document).ready(function() {
   $('form#numbers').submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($('input#inputNumbers').val());
-    $('.output#result').text(beepboop(userInput));
-
-
+    $('#result').text(beepBoop(userInput));
   });
 });
