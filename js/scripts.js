15c3d8aaee7ced24ef5e9760d49109274283acfb
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 12:38:40 -0700

    add css functionality to hide/show results

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
-//Back-end logic//
+//Back-End logic//
 function beepBoop(userInput) {
   var outputArray = []
 
-  for (i = 0; i <= userInput; i ++) {
-    if ([i].toString().includes(3)) {
+  for (i = 0; i <= userInput; i++) {
+    if (i.toString().includes(3)) {
       outputArray.push("I'm sorry, Dave. I'm afraid I can't do that. ")
-    } else if ([i].toString().includes(2)) {
-      outputArray.push("Boop! ")
-    } else if ([i].toString().includes(1)) {
-      outputArray.push("Beep! ")
+    } else if (i.toString().includes(2)) {
+      outputArray.push("Boop! ");
+    } else if (i.toString().includes(1)) {
+      outputArray.push("Beep! ");
     } else {
       outputArray.push(i + ", ");
     }
   }
   return outputArray;
 }
-//front-end logic
+//Front-End logic//
 $(document).ready(function() {
   $('form#numbers').submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($('input#inputNumbers').val());
     $('#results').append(beepBoop(userInput));
+    $('#output').show();
   });
 });
