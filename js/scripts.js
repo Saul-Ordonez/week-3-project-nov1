5fe767ac8b900603eb905523c16a0b55ec942ac6
Saul Ordonez <sauleordonez@gmail.com>
2019-11-01 12:08:08 -0700

    fix branch issue to get working code

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,25 @@
-//back-end logic
-// function inputCheck(userInput) {
-//   if (isNaN(userInput)) {
-//     alert('Please enter a real number');
-//   }
-//   else if (userInput < 0) {
-//     alert('Please enter a positive number');
-//   }
-// }
+//Back-end logic//
 function beepBoop(userInput) {
-  // var beep = 'Beep!';
-  // var boop = 'Boop!';
-  // var sorry = "I'm sorry Dave, I'm afraid I cannot do that.";
-  var outputArray = [];
+  var outputArray = []
 
-  for (i = 0; i < userInput.length; i++); {
-    if ((i.toString()).includes("3")) {
-      outputArray.push(" I'm sorry Dave, I'm afraid I cannot do that. ");
-    }
-    else if ((i.toString()).includes("2")) {
-      outputArray.push(' boop! ');
-    }
-    else if ((i.toString()).includes("1")) {
-      outputArray.push(' beep! ');
-    }
-    else {
-      outputArray.push(i);
+  for (i = 0; i <= userInput; i ++) {
+    if ([i].toString().includes(3)) {
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that. ")
+    } else if ([i].toString().includes(2)) {
+      outputArray.push("Boop! ")
+    } else if ([i].toString().includes(1)) {
+      outputArray.push("Beep! ")
+    } else {
+      outputArray.push(i + ", ");
     }
   }
-  return outputArray.join();
-
+  return outputArray;
 }
-
 //front-end logic
 $(document).ready(function() {
   $('form#numbers').submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($('input#inputNumbers').val());
-    $('#results').text(beepBoop(userInput));
+    $('#results').append(beepBoop(userInput));
   });
 });
